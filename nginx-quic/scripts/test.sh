b8c5e6082b823518ee35c91cb1394d181ea4db60
bpq233 <1134277524@qq.com>
2023-05-11 18:37:05 +0800

    simple case

--- nginx-quic/scripts/test.sh
+++ nginx-quic/scripts/test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+script_dir=$(dirname "$0")
+cd $script_dir/../
+
+sudo ls > /dev/null 2>&1
+
 sudo sh -c "exec ./bin/nginx -C BBR  > /dev/null 2>&1" &
 pid=$!
 ./bin/quic_client https://test.bpqiang.cloud
