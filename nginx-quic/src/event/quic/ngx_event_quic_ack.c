6fc7653965d30bd5f967dc3f01459c6f7f921c24
bpq233 <1134277524@qq.com>
2023-05-11 21:56:07 +0800

    inital commit

--- nginx-quic/src/event/quic/ngx_event_quic_ack.c
+++ nginx-quic/src/event/quic/ngx_event_quic_ack.c
@@ -89,8 +89,9 @@ ngx_quic_handle_ack_frame(ngx_connection_t *c, ngx_quic_header_t *pkt,
     qc = ngx_quic_get_connection(c);
     ngx_quic_congestion_t  *cg = &qc->congestion;
 
-    cg->prior_delivered = cg->delivered;
     cg->prior_in_flight = cg->in_flight;
+    cg->prior_delivered = cg->delivered;
+    
 
     ctx = ngx_quic_get_send_ctx(qc, pkt->level);
 
