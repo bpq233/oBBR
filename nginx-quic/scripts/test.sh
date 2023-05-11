3d49e93127ec5a1957254c424e793d22c24357e2
bpq233 <1134277524@qq.com>
2023-05-11 21:05:10 +0800

    figs

--- nginx-quic/scripts/test.sh
+++ nginx-quic/scripts/test.sh
@@ -2,7 +2,11 @@
 script_dir=$(dirname "$0")
 cd $script_dir/../
 
-sudo ls > /dev/null 2>&1
+dev=lo
+
+sudo tc qdisc add dev $dev root handle 1:0 netem delay 50ms
+sudo tc qdisc add dev $dev parent 1:1 handle 10: tbf rate 40Mbit burst 100KB limit 250KB
+sudo tc qdisc del dev $dev root
 
 sudo sh -c "exec ./bin/nginx -C BBR  > /dev/null 2>&1" &
 pid=$!
