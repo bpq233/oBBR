b2a8c2573a4ef78d2f5bc8fda7b69a14bf304844
bpq233 <84391336+bpq233@users.noreply.github.com>
2023-06-19 21:58:08 +0800

    change

--- nginx-quic/scripts/nets/change.sh
+++ nginx-quic/scripts/nets/change.sh
@@ -2,8 +2,8 @@
 dev=lo
 while true
 do
-	sudo tc qdisc change dev $dev parent 1:1 handle 10: tbf rate $1Mbit burst $5K limit $3KB
+	sudo tc qdisc change dev $dev parent 1:1 handle 10: tbf rate $1Mbit burst $5KB limit $3KB
 	sleep $4
-	sudo tc qdisc change dev $dev parent 1:1 handle 10: tbf rate $2Mbit burst $5K limit $3KB
+	sudo tc qdisc change dev $dev parent 1:1 handle 10: tbf rate $2Mbit burst $5KB limit $3KB
 	sleep $4
 done
